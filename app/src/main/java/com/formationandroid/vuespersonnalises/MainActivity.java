72680d9ad541bf7064d809386b180d3c6eb09f47
mregnauld <mregnauld@gmail.com>
2020-01-12 19:57:42 +0100

    Mise a jour Android 10

--- app/src/main/java/com/formationandroid/vuespersonnalises/MainActivity.java
+++ app/src/main/java/com/formationandroid/vuespersonnalises/MainActivity.java
@@ -1,9 +1,10 @@
 package com.formationandroid.vuespersonnalises;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener
 {
